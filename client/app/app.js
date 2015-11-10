30cb3939c30584567456b2e51200f8dd18a9c1e9
Pokegear <christoph.ding@gmail.com>
2015-11-09 19:04:02 -0800

    Got rid of 'submit' button.  Text box clears itself upon submit.

--- client/app/app.js
+++ client/app/app.js
@@ -3,8 +3,38 @@ var app = angular.module('toDone', [])
 app.controller('taskListCtrl', function($scope) {
   $scope.tasks = [];
   $scope.counter = 5;
-  $scope.addTask = function() {    
-    $scope.tasks.push($scope.task);    
+  
+  $scope.calculateCounter = function() {
+    $scope.counter = 5 - $scope.tasks.length;
   }
+  $scope.addTask = function() {    
+    if ($scope.counter > 0) {
+      $scope.tasks.push($scope.task);  
+      $scope.task = ''
+      $scope.calculateCounter();
+    }
+  }
+  $scope.deleteTask = function(index) {
+    $scope.tasks.splice(index, 1);
+    $scope.calculateCounter();
+  }
+
 });
+
+//   <h2>Add contact</h2>
+//   <form ng-submit="addMail()">
+//     <input type="mail" ng-model="mailAdress"/>
+//     <button type="submit">Add contact</button>
+//   </form>
+// </div>
+
+  
+//   $scope.addMail = function() {
+//     if(this.mailAdress) {
+//       $scope.contacts.push($scope.mailAdress);
+//       $scope.mailAdress = "";
+//     }
+//   };
+// }
+
 
