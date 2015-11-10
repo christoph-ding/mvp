6732d1cc01176416b9b3f0ad7d2fd7735bb41c85
Pokegear <christoph.ding@gmail.com>
2015-11-10 14:32:53 -0800

    Pretty much done with front-end.  Cleaned up some styles.  Added 'click' to chip away feature.  Starting database now.

--- client/app/app.js
+++ client/app/app.js
@@ -8,7 +8,12 @@ app.controller('taskListCtrl', function($scope) {
   $scope.graphWidth = 500; 
   $scope.graphHeight = 350;
 
-  $scope.colorBank = []
+  $scope.colorBank = ['#F0A202', '#F18805', '#D95D39', '#202C59', '#E71D36',
+    '#381D2A', '#C32F27', '#780116', '#084887', '#BCE784', '#525174', '#348AA7',
+    '#841C26', '#8FB8DE', '#E0F47F', '#A9FDAC', '#44CF6C', '#32A287', '#8EA604',
+    '#FF4E00', '#F5BB00', '#EC9F05', '#BF3100', '#D36135', '#A24936', '#46B1C9',
+    '#720026', '#BA5624', '#90FCF9'   
+  ]
 
   $scope.calculateCounter = function() {
     $scope.counter = 5 - $scope.tasks.length;
@@ -24,7 +29,8 @@ app.controller('taskListCtrl', function($scope) {
 
   $scope.addTask = function() {    
     if ($scope.counter > 0 && document.querySelector('#taskText').value !== '') {
-      var taskObj = {toDo:$scope.task, weight:0}
+      var taskColor = $scope.colorBank[Math.floor(Math.random() * $scope.colorBank.length)];
+      var taskObj = {toDo:$scope.task, color:taskColor, height:350}
       $scope.tasks.push(taskObj);  
       document.querySelector('#taskText').value = '';
       $scope.calculateCounter();
@@ -34,12 +40,16 @@ app.controller('taskListCtrl', function($scope) {
     }
   }
 
+  $scope.chipAway = function(index) {
+    $scope.tasks[index]['height'] = $scope.tasks[index]['height'] - 35;
+    if ($scope.tasks[index]['height'] === 0) {
+      $scope.deleteTask(index);      
+    }
+  } 
+
   $scope.deleteTask = function(index) {
     $scope.tasks.splice(index, 1);
     $scope.calculateCounter();
   }
-
 });
-
-
 
--- server/server.js
+++ server/server.js
@@ -13,8 +13,6 @@ app.get('/', function(req, res) {
   res.send(path.resolve(__dirname + '/../client/app/index.html'))
 });
 
-
-
 app.listen(8000);
 console.log("Listening in on port 8000")
 module.exports = app;
